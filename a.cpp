07135606a95ef348ef5897eae1a5ec8caea127a0
Pawan Jindal <44540309+jindalpawan@users.noreply.github.com>
2020-10-01 15:09:00 +0530

    update

--- a.cpp
+++ a.cpp
@@ -1,45 +1,6 @@
-/*     ******************* PPPPPPPPPPPPPPP                      JJJJJJJJJJJJJJJJJJJJJJJ
-     ******************* PPPPPPPPPPPPPPP                                    JJ
-     ******************* PP           PP                                    JJ
-     ******************* PP           PP                                    JJ
-     ******************* PP           PP                                    JJ
-     ******************* PP           PP                                    JJ
-     ******************* PPPPPPPPPPPPPPP                                    JJ
-     ******************* PPPPPPPPPPPPPPP                                    JJ
-     ******************* PP                                                 JJ
-     ******************* PP                                                 JJ
-     ******************* PP                                                 JJ
-     ******************* PP                                    JJ           JJ
-     ******************* PP                                     JJ          JJ
-     ******************* PP                     .....            JJ         JJ
-     ******************* PP                     .....             JJ        JJ
-     ******************* PP                     .....              JJJJJJJJJJJ
-*/
+
 #include <bits/stdc++.h>
 using namespace std;
-/*
-#define fs first
-#define sc second
-#define pb push_back
-#define eb emplace_back
-#define begin_end(a) a.begin(),a.end()
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-using namespace std;
-*/
-//int Binser(int Start,int S){
-//// if(S<0) return -1;
-//   int Le=Start+1,Ri=N,Mid,Save=0;
-//   while(Le<=Ri){
-//        Mid=(Le+Ri)/2;
-//        if(S>=Pref[Mid]-Pref[Start]){
-//             Save=Mid;
-//             Le=Mid+1;
-//        } else Ri=Mid-1;
-//   }
-//   return Save;
-//}
 #define forl(i,a,n) for(int i=a;i<n;i++)
 #define rfor(i,n,a) for(int i=n;i>=a;i--)
 #define fore(i,a,n) for(int i=a;i<=n;i++)
@@ -115,7 +76,6 @@ int main()
     int countings=0;
     while(!m.count(p2))
     {
-        //cout<<addition(p1,p2);
         countings++;
         int f=0;
         for(int i=2;i*i<=p2;i++)
@@ -131,9 +91,7 @@ int main()
             p2=p2/f;
         else
             p2=1;
-        //cout<<p2<<" ";
     }
-    //cout<<countings;
     cout<<countings+m[p2];
     return 0;
 }   
@@ -142,7 +100,6 @@ void swap(int *p1, int *p2)
     int temp = *p1; 
     *p1 = *p2; 
     *p2 = temp; 
-    //cout<<temp;
 } 
 
 void bubbleSort(int arr[], int n)  
